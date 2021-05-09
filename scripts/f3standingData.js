2ebd50aee5887a7298b41a8feabfe8cbeaba51dc
GitHub Action <action@github.com>
2021-05-09 00:05:16 +0000

    update table data

--- scripts/f3standingData.js
+++ scripts/f3standingData.js
@@ -1,10 +1,242 @@
-var tabledata=`<tbody>
-	<td colspan='100' style='text-align:center'>The 2021 season is not currently in session</td>
-</tbody>
+var tabledata=`<table border="1" class="dataframe">
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>O. Caldwell</td>
+      <td>PREMA Racing</td>
+      <td>20</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>C. Novalak</td>
+      <td>Trident</td>
+      <td>20</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>A. Smolyar</td>
+      <td>ART Grand Prix</td>
+      <td>17</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>C. Collet</td>
+      <td>MP Motorsport</td>
+      <td>16</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>V. Martins</td>
+      <td>MP Motorsport</td>
+      <td>14</td>
+    </tr>
+    <tr>
+      <td>6</td>
+      <td>F. Vesti</td>
+      <td>ART Grand Prix</td>
+      <td>14</td>
+    </tr>
+    <tr>
+      <td>7</td>
+      <td>J. Edgar</td>
+      <td>Carlin Buzz Racing</td>
+      <td>11</td>
+    </tr>
+    <tr>
+      <td>8</td>
+      <td>L. Sargeant</td>
+      <td>Charouz Racing System</td>
+      <td>8</td>
+    </tr>
+    <tr>
+      <td>9</td>
+      <td>A. Iwasa</td>
+      <td>Hitech Grand Prix</td>
+      <td>4</td>
+    </tr>
+    <tr>
+      <td>10</td>
+      <td>J. Doohan</td>
+      <td>Trident</td>
+      <td>3</td>
+    </tr>
+    <tr>
+      <td>11</td>
+      <td>D. Hauger</td>
+      <td>PREMA Racing</td>
+      <td>3</td>
+    </tr>
+    <tr>
+      <td>12</td>
+      <td>J. Crawford</td>
+      <td>Hitech Grand Prix</td>
+      <td>2</td>
+    </tr>
+    <tr>
+      <td>13</td>
+      <td>J. Correa</td>
+      <td>ART Grand Prix</td>
+      <td>1</td>
+    </tr>
+    <tr>
+      <td>14</td>
+      <td>M. Nannini</td>
+      <td>HWA RACELAB</td>
+      <td>1</td>
+    </tr>
+    <tr>
+      <td>15</td>
+      <td>C. Williams</td>
+      <td>Jenzer Motorsport</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>16</td>
+      <td>D. Schumacher</td>
+      <td>Trident</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>17</td>
+      <td>R. Stanek</td>
+      <td>Hitech Grand Prix</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>18</td>
+      <td>E. Fittipaldi</td>
+      <td>Charouz Racing System</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>19</td>
+      <td>R. de Gerus</td>
+      <td>Charouz Racing System</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>20</td>
+      <td>P. Chovet</td>
+      <td>Jenzer Motorsport</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>21</td>
+      <td>T. van der Helm</td>
+      <td>MP Motorsport</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>22</td>
+      <td>A. Cordeel</td>
+      <td>Campos Racing</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>23</td>
+      <td>K. Frederick</td>
+      <td>Carlin Buzz Racing</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>24</td>
+      <td>R. Villagómez</td>
+      <td>HWA RACELAB</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>25</td>
+      <td>O. Rasmussen</td>
+      <td>HWA RACELAB</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>26</td>
+      <td>I. Cohen</td>
+      <td>Carlin Buzz Racing</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>27</td>
+      <td>F. Ugran</td>
+      <td>Jenzer Motorsport</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>28</td>
+      <td>L. Colombo</td>
+      <td>Campos Racing</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>29</td>
+      <td>L. Tóth</td>
+      <td>Campos Racing</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>30</td>
+      <td>A. Leclerc</td>
+      <td>PREMA Racing</td>
+      <td>0</td>
+    </tr>
+  </tbody>
+</table>
 `;
 document.getElementById('f3-drivers').innerHTML+=tabledata;
-var tabledata=`<tbody>
-	<td colspan='100' style='text-align:center'>The 2021 season is not currently in session</td>
-</tbody>
+var tabledata=`<table border="1" class="dataframe">
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>ART Grand Prix</td>
+      <td>32</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>MP</td>
+      <td>30</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>PREMA</td>
+      <td>23</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>Trident</td>
+      <td>23</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>Carlin</td>
+      <td>11</td>
+    </tr>
+    <tr>
+      <td>6</td>
+      <td>Charouz</td>
+      <td>8</td>
+    </tr>
+    <tr>
+      <td>7</td>
+      <td>Hitech</td>
+      <td>6</td>
+    </tr>
+    <tr>
+      <td>8</td>
+      <td>HWA RACELAB</td>
+      <td>1</td>
+    </tr>
+    <tr>
+      <td>9</td>
+      <td>Jenzer</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>10</td>
+      <td>Campos</td>
+      <td>0</td>
+    </tr>
+  </tbody>
+</table>
 `;
 document.getElementById('f3-constructors').innerHTML+=tabledata;
