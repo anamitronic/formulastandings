e52299d28224041c2328a84468c157dfdfa8f024
GitHub Action <action@github.com>
2021-05-23 00:18:35 +0000

    update table data

--- scripts/f1standingData.js
+++ scripts/f1standingData.js
@@ -180,4 +180,4 @@ var tabledata=`<table border="1" class="dataframe">
 </table>
 `;
 document.getElementById('f1-constructors').innerHTML+=tabledata;
-document.getElementById('lastup').innerHTML='17/05/2021 23:44:34 UTC';
+document.getElementById('lastup').innerHTML='23/05/2021 00:18:17 UTC';
--- scripts/f2standingData.js
+++ scripts/f2standingData.js
@@ -4,132 +4,132 @@ var tabledata=`<table border="1" class="dataframe">
       <td>1</td>
       <td>G. Zhou</td>
       <td>UNI-Virtuosi</td>
-      <td>41</td>
+      <td>68</td>
     </tr>
     <tr>
       <td>2</td>
-      <td>L. Lawson</td>
-      <td>Hitech Grand Prix</td>
-      <td>30</td>
+      <td>O. Piastri</td>
+      <td>PREMA Racing</td>
+      <td>52</td>
     </tr>
     <tr>
       <td>3</td>
-      <td>J. Daruvala</td>
-      <td>Carlin</td>
-      <td>28</td>
+      <td>T. Pourchaire</td>
+      <td>ART Grand Prix</td>
+      <td>47</td>
     </tr>
     <tr>
       <td>4</td>
-      <td>O. Piastri</td>
-      <td>PREMA Racing</td>
-      <td>21</td>
+      <td>D. Ticktum</td>
+      <td>Carlin</td>
+      <td>38</td>
     </tr>
     <tr>
       <td>5</td>
-      <td>D. Ticktum</td>
-      <td>Carlin</td>
-      <td>19</td>
+      <td>L. Lawson</td>
+      <td>Hitech Grand Prix</td>
+      <td>36</td>
     </tr>
     <tr>
       <td>6</td>
-      <td>R. Verschoor</td>
-      <td>MP Motorsport</td>
-      <td>18</td>
+      <td>R. Shwartzman</td>
+      <td>PREMA Racing</td>
+      <td>30</td>
     </tr>
     <tr>
       <td>7</td>
+      <td>F. Drugovich</td>
+      <td>UNI-Virtuosi</td>
+      <td>29</td>
+    </tr>
+    <tr>
+      <td>8</td>
+      <td>J. Daruvala</td>
+      <td>Carlin</td>
+      <td>29</td>
+    </tr>
+    <tr>
+      <td>9</td>
+      <td>R. Verschoor</td>
+      <td>MP Motorsport</td>
+      <td>23</td>
+    </tr>
+    <tr>
+      <td>10</td>
+      <td>J. Vips</td>
+      <td>Hitech Grand Prix</td>
+      <td>22</td>
+    </tr>
+    <tr>
+      <td>11</td>
+      <td>R. Boschung</td>
+      <td>Campos Racing</td>
+      <td>22</td>
+    </tr>
+    <tr>
+      <td>12</td>
       <td>C. Lundgaard</td>
       <td>ART Grand Prix</td>
       <td>16</td>
     </tr>
     <tr>
-      <td>8</td>
-      <td>R. Shwartzman</td>
-      <td>PREMA Racing</td>
-      <td>16</td>
-    </tr>
-    <tr>
-      <td>9</td>
+      <td>13</td>
       <td>D. Beckmann</td>
       <td>Charouz Racing System</td>
       <td>12</td>
     </tr>
     <tr>
-      <td>10</td>
+      <td>14</td>
+      <td>R. Nissany</td>
+      <td>DAMS</td>
+      <td>12</td>
+    </tr>
+    <tr>
+      <td>15</td>
       <td>M. Armstrong</td>
       <td>DAMS</td>
       <td>10</td>
     </tr>
     <tr>
-      <td>11</td>
-      <td>T. Pourchaire</td>
-      <td>ART Grand Prix</td>
-      <td>8</td>
-    </tr>
-    <tr>
-      <td>12</td>
-      <td>F. Drugovich</td>
-      <td>UNI-Virtuosi</td>
-      <td>2</td>
-    </tr>
-    <tr>
-      <td>13</td>
+      <td>16</td>
       <td>L. Zendeli</td>
       <td>MP Motorsport</td>
-      <td>2</td>
+      <td>4</td>
     </tr>
     <tr>
-      <td>14</td>
+      <td>17</td>
       <td>M. Sato</td>
       <td>Trident</td>
       <td>1</td>
     </tr>
     <tr>
-      <td>15</td>
-      <td>M. Nannini</td>
+      <td>19</td>
+      <td>J. Aitken</td>
       <td>HWA RACELAB</td>
-      <td>1</td>
-    </tr>
-    <tr>
-      <td>16</td>
-      <td>J. Vips</td>
-      <td>Hitech Grand Prix</td>
       <td>0</td>
     </tr>
     <tr>
-      <td>17</td>
-      <td>G. Samaia</td>
-      <td>Charouz Racing System</td>
-      <td>0</td>
-    </tr>
-    <tr>
-      <td>18</td>
+      <td>20</td>
       <td>B. Viscaal</td>
       <td>Trident</td>
       <td>0</td>
     </tr>
     <tr>
-      <td>19</td>
-      <td>R. Nissany</td>
-      <td>DAMS</td>
-      <td>0</td>
-    </tr>
-    <tr>
-      <td>20</td>
-      <td>G. Petecof</td>
-      <td>Campos Racing</td>
-      <td>0</td>
-    </tr>
-    <tr>
       <td>21</td>
-      <td>R. Boschung</td>
-      <td>Campos Racing</td>
+      <td>G. Samaia</td>
+      <td>Charouz Racing System</td>
       <td>0</td>
     </tr>
     <tr>
       <td>22</td>
       <td>A. Deledda</td>
       <td>HWA RACELAB</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>23</td>
+      <td>G. Petecof</td>
+      <td>Campos Racing</td>
       <td>0</td>
     </tr>
   </tbody>
@@ -140,58 +140,58 @@ var tabledata=`<table border="1" class="dataframe">
   <tbody>
     <tr>
       <td>1</td>
-      <td>Carlin</td>
-      <td>47</td>
+      <td>UNI-Virtuosi</td>
+      <td>97</td>
     </tr>
     <tr>
       <td>2</td>
-      <td>UNI-Virtuosi</td>
-      <td>43</td>
+      <td>PREMA</td>
+      <td>82</td>
     </tr>
     <tr>
       <td>3</td>
-      <td>PREMA</td>
-      <td>37</td>
+      <td>Carlin</td>
+      <td>67</td>
     </tr>
     <tr>
       <td>4</td>
-      <td>Hitech</td>
-      <td>30</td>
+      <td>ART</td>
+      <td>63</td>
     </tr>
     <tr>
       <td>5</td>
-      <td>ART</td>
-      <td>24</td>
+      <td>Hitech</td>
+      <td>58</td>
     </tr>
     <tr>
       <td>6</td>
       <td>MP</td>
-      <td>20</td>
+      <td>27</td>
     </tr>
     <tr>
       <td>7</td>
+      <td>DAMS</td>
+      <td>22</td>
+    </tr>
+    <tr>
+      <td>8</td>
+      <td>Campos</td>
+      <td>22</td>
+    </tr>
+    <tr>
+      <td>9</td>
       <td>Charouz</td>
       <td>12</td>
     </tr>
     <tr>
-      <td>8</td>
-      <td>DAMS</td>
       <td>10</td>
-    </tr>
-    <tr>
-      <td>9</td>
       <td>Trident</td>
       <td>1</td>
     </tr>
     <tr>
-      <td>10</td>
+      <td>11</td>
       <td>HWA</td>
       <td>1</td>
-    </tr>
-    <tr>
-      <td>11</td>
-      <td>Campos</td>
-      <td>0</td>
     </tr>
   </tbody>
 </table>
