c4a0ce472cfc303ea745f95c85f7d12eda09c902
GitHub Action <action@github.com>
2021-08-02 16:03:19 +0000

    update table data

--- scripts/f1standingData.js
+++ scripts/f1standingData.js
@@ -4,13 +4,13 @@ var tabledata=`<table border="1" class="dataframe">
       <td>1</td>
       <td>Lewis Hamilton HAM</td>
       <td>Mercedes</td>
-      <td>192</td>
+      <td>195</td>
     </tr>
     <tr>
       <td>2</td>
       <td>Max Verstappen VER</td>
       <td>Red Bull Racing Honda</td>
-      <td>186</td>
+      <td>187</td>
     </tr>
     <tr>
       <td>3</td>
@@ -32,75 +32,69 @@ var tabledata=`<table border="1" class="dataframe">
     </tr>
     <tr>
       <td>6</td>
+      <td>Carlos Sainz SAI</td>
+      <td>Ferrari</td>
+      <td>83</td>
+    </tr>
+    <tr>
+      <td>7</td>
       <td>Charles Leclerc LEC</td>
       <td>Ferrari</td>
       <td>80</td>
     </tr>
     <tr>
-      <td>7</td>
-      <td>Carlos Sainz SAI</td>
-      <td>Ferrari</td>
-      <td>80</td>
+      <td>8</td>
+      <td>Pierre Gasly GAS</td>
+      <td>AlphaTauri Honda</td>
+      <td>50</td>
     </tr>
     <tr>
-      <td>8</td>
+      <td>9</td>
       <td>Daniel Ricciardo RIC</td>
       <td>McLaren Mercedes</td>
       <td>50</td>
     </tr>
     <tr>
-      <td>9</td>
-      <td>Sebastian Vettel VET</td>
-      <td>Aston Martin Mercedes</td>
-      <td>48</td>
-    </tr>
-    <tr>
       <td>10</td>
-      <td>Pierre Gasly GAS</td>
-      <td>AlphaTauri Honda</td>
-      <td>48</td>
-    </tr>
-    <tr>
-      <td>11</td>
       <td>Esteban Ocon OCO</td>
       <td>Alpine Renault</td>
       <td>39</td>
     </tr>
     <tr>
-      <td>12</td>
+      <td>11</td>
       <td>Fernando Alonso ALO</td>
       <td>Alpine Renault</td>
-      <td>36</td>
+      <td>38</td>
     </tr>
     <tr>
       <td>13</td>
+      <td>Yuki Tsunoda TSU</td>
+      <td>AlphaTauri Honda</td>
+      <td>18</td>
+    </tr>
+    <tr>
+      <td>14</td>
       <td>Lance Stroll STR</td>
       <td>Aston Martin Mercedes</td>
       <td>18</td>
     </tr>
     <tr>
-      <td>14</td>
-      <td>Yuki Tsunoda TSU</td>
-      <td>AlphaTauri Honda</td>
-      <td>16</td>
-    </tr>
-    <tr>
       <td>15</td>
       <td>Nicholas Latifi LAT</td>
       <td>Williams Mercedes</td>
-      <td>4</td>
+      <td>6</td>
     </tr>
     <tr>
       <td>16</td>
       <td>George Russell RUS</td>
       <td>Williams Mercedes</td>
-      <td>2</td>
+      <td>4</td>
     </tr>
     <tr>
       <td>17</td>
       <td>Kimi Räikkönen RAI</td>
       <td>Alfa Romeo Racing Ferrari</td>
-      <td>1</td>
+      <td>2</td>
     </tr>
     <tr>
       <td>18</td>
@@ -129,47 +123,47 @@ var tabledata=`<table border="1" class="dataframe">
     <tr>
       <td>1</td>
       <td>Mercedes</td>
-      <td>300</td>
+      <td>303</td>
     </tr>
     <tr>
       <td>2</td>
       <td>Red Bull Racing Honda</td>
-      <td>290</td>
+      <td>291</td>
     </tr>
     <tr>
       <td>3</td>
-      <td>McLaren Mercedes</td>
+      <td>Ferrari</td>
       <td>163</td>
     </tr>
     <tr>
       <td>4</td>
-      <td>Ferrari</td>
-      <td>160</td>
+      <td>McLaren Mercedes</td>
+      <td>163</td>
     </tr>
     <tr>
       <td>5</td>
       <td>Alpine Renault</td>
-      <td>75</td>
+      <td>77</td>
     </tr>
     <tr>
       <td>6</td>
-      <td>Aston Martin Mercedes</td>
-      <td>66</td>
+      <td>AlphaTauri Honda</td>
+      <td>68</td>
     </tr>
     <tr>
       <td>7</td>
-      <td>AlphaTauri Honda</td>
-      <td>64</td>
+      <td>Aston Martin Mercedes</td>
+      <td>48</td>
     </tr>
     <tr>
       <td>8</td>
       <td>Williams Mercedes</td>
-      <td>6</td>
+      <td>10</td>
     </tr>
     <tr>
       <td>9</td>
       <td>Alfa Romeo Racing Ferrari</td>
-      <td>2</td>
+      <td>3</td>
     </tr>
     <tr>
       <td>10</td>
@@ -180,4 +174,4 @@ var tabledata=`<table border="1" class="dataframe">
 </table>
 `;
 document.getElementById('f1-constructors').innerHTML+=tabledata;
-document.getElementById('lastup').innerHTML='02/08/2021 15:40:42 UTC';
+document.getElementById('lastup').innerHTML='02/08/2021 16:03:06 UTC';
