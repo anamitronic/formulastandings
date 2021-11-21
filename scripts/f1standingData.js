b13a14766e64bbd6691771980f5e4cf5d2df0fee
GitHub Action <action@github.com>
2021-11-21 15:37:43 +0000

    update table data

--- scripts/f1standingData.js
+++ scripts/f1standingData.js
@@ -4,13 +4,13 @@ var tabledata=`<table border="1" class="dataframe">
       <td>1</td>
       <td>Max Verstappen VER</td>
       <td>Red Bull Racing Honda</td>
-      <td>332.5</td>
+      <td>351.5</td>
     </tr>
     <tr>
       <td>2</td>
       <td>Lewis Hamilton HAM</td>
       <td>Mercedes</td>
-      <td>318.5</td>
+      <td>343.5</td>
     </tr>
     <tr>
       <td>3</td>
@@ -22,25 +22,25 @@ var tabledata=`<table border="1" class="dataframe">
       <td>4</td>
       <td>Sergio Perez PER</td>
       <td>Red Bull Racing Honda</td>
-      <td>178</td>
+      <td>190</td>
     </tr>
     <tr>
       <td>5</td>
       <td>Lando Norris NOR</td>
       <td>McLaren Mercedes</td>
-      <td>151</td>
+      <td>153</td>
     </tr>
     <tr>
       <td>6</td>
       <td>Charles Leclerc LEC</td>
       <td>Ferrari</td>
-      <td>148</td>
+      <td>152</td>
     </tr>
     <tr>
       <td>7</td>
       <td>Carlos Sainz SAI</td>
       <td>Ferrari</td>
-      <td>139.5</td>
+      <td>145.5</td>
     </tr>
     <tr>
       <td>8</td>
@@ -58,25 +58,25 @@ var tabledata=`<table border="1" class="dataframe">
       <td>10</td>
       <td>Fernando Alonso ALO</td>
       <td>Alpine Renault</td>
-      <td>62</td>
+      <td>77</td>
     </tr>
     <tr>
       <td>11</td>
       <td>Esteban Ocon OCO</td>
       <td>Alpine Renault</td>
-      <td>50</td>
+      <td>60</td>
     </tr>
     <tr>
       <td>12</td>
       <td>Sebastian Vettel VET</td>
       <td>Aston Martin Mercedes</td>
-      <td>42</td>
+      <td>43</td>
     </tr>
     <tr>
       <td>13</td>
       <td>Lance Stroll STR</td>
       <td>Aston Martin Mercedes</td>
-      <td>26</td>
+      <td>34</td>
     </tr>
     <tr>
       <td>14</td>
@@ -186,4 +186,4 @@ var tabledata=`<table border="1" class="dataframe">
 </table>
 `;
 document.getElementById('f1-constructors').innerHTML+=tabledata;
-document.getElementById('lastup').innerHTML='21/11/2021 15:02:01 UTC';
+document.getElementById('lastup').innerHTML='21/11/2021 15:37:34 UTC';
