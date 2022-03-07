fcd2bd6108764d5b571123dc3f564a2f5a103260
GitHub Action <action@github.com>
2022-03-07 09:08:08 +0000

    update table data

--- scripts/f3standingData.js
+++ scripts/f3standingData.js
@@ -1,12 +1,10 @@
-var tabledata=`<table border="1" class="dataframe">
-  <tbody>
-  </tbody>
-</table>
+var tabledata=`<tbody>
+	<td colspan='100' style='text-align:center'>The 2022 season is not currently in session</td>
+</tbody>
 `;
 document.getElementById('f3-drivers').innerHTML+=tabledata;
-var tabledata=`<table border="1" class="dataframe">
-  <tbody>
-  </tbody>
-</table>
+var tabledata=`<tbody>
+	<td colspan='100' style='text-align:center'>The 2022 season is not currently in session</td>
+</tbody>
 `;
 document.getElementById('f3-constructors').innerHTML+=tabledata;
