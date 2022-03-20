2c81ed323637e30e3b2c18c59da3c028517db0f1
GitHub Action <action@github.com>
2022-03-20 16:54:57 +0000

    update table data

--- scripts/f1standingData.js
+++ scripts/f1standingData.js
@@ -1,11 +1,177 @@
-var tabledata=`<tbody>
-	<td colspan='100' style='text-align:center'>The 2022 season is not currently in session</td>
-</tbody>
+var tabledata=`<table border="1" class="dataframe">
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Charles Leclerc LEC</td>
+      <td>Ferrari</td>
+      <td>26</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Carlos Sainz SAI</td>
+      <td>Ferrari</td>
+      <td>18</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Lewis Hamilton HAM</td>
+      <td>Mercedes</td>
+      <td>15</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>George Russell RUS</td>
+      <td>Mercedes</td>
+      <td>12</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>Kevin Magnussen MAG</td>
+      <td>Haas Ferrari</td>
+      <td>10</td>
+    </tr>
+    <tr>
+      <td>6</td>
+      <td>Valtteri Bottas BOT</td>
+      <td>Alfa Romeo Ferrari</td>
+      <td>8</td>
+    </tr>
+    <tr>
+      <td>7</td>
+      <td>Esteban Ocon OCO</td>
+      <td>Alpine Renault</td>
+      <td>6</td>
+    </tr>
+    <tr>
+      <td>8</td>
+      <td>Yuki Tsunoda TSU</td>
+      <td>AlphaTauri RBPT</td>
+      <td>4</td>
+    </tr>
+    <tr>
+      <td>9</td>
+      <td>Fernando Alonso ALO</td>
+      <td>Alpine Renault</td>
+      <td>2</td>
+    </tr>
+    <tr>
+      <td>10</td>
+      <td>Zhou Guanyu ZHO</td>
+      <td>Alfa Romeo Ferrari</td>
+      <td>1</td>
+    </tr>
+    <tr>
+      <td>11</td>
+      <td>Mick Schumacher MSC</td>
+      <td>Haas Ferrari</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>12</td>
+      <td>Lance Stroll STR</td>
+      <td>Aston Martin Aramco Mercedes</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>13</td>
+      <td>Alexander Albon ALB</td>
+      <td>Williams Mercedes</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>14</td>
+      <td>Daniel Ricciardo RIC</td>
+      <td>McLaren Mercedes</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>15</td>
+      <td>Lando Norris NOR</td>
+      <td>McLaren Mercedes</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>16</td>
+      <td>Nicholas Latifi LAT</td>
+      <td>Williams Mercedes</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>17</td>
+      <td>Nico Hulkenberg HUL</td>
+      <td>Aston Martin Aramco Mercedes</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>18</td>
+      <td>Sergio Perez PER</td>
+      <td>Red Bull Racing RBPT</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>19</td>
+      <td>Max Verstappen VER</td>
+      <td>Red Bull Racing RBPT</td>
+      <td>0</td>
+    </tr>
+  </tbody>
+</table>
 `;
 document.getElementById('f1-drivers').innerHTML+=tabledata;
-var tabledata=`<tbody>
-	<td colspan='100' style='text-align:center'>The 2022 season is not currently in session</td>
-</tbody>
+var tabledata=`<table border="1" class="dataframe">
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Ferrari</td>
+      <td>44</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Mercedes</td>
+      <td>27</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Haas Ferrari</td>
+      <td>10</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>Alfa Romeo Ferrari</td>
+      <td>9</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>Alpine Renault</td>
+      <td>8</td>
+    </tr>
+    <tr>
+      <td>6</td>
+      <td>AlphaTauri RBPT</td>
+      <td>4</td>
+    </tr>
+    <tr>
+      <td>7</td>
+      <td>Aston Martin Aramco Mercedes</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>8</td>
+      <td>Williams Mercedes</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>9</td>
+      <td>McLaren Mercedes</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>10</td>
+      <td>Red Bull Racing RBPT</td>
+      <td>0</td>
+    </tr>
+  </tbody>
+</table>
 `;
 document.getElementById('f1-constructors').innerHTML+=tabledata;
-document.getElementById('lastup').innerHTML='20/03/2022 16:05:08 UTC';
+document.getElementById('lastup').innerHTML='20/03/2022 16:54:45 UTC';
