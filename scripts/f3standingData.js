ec244c0d8b28d176043d98fdca3478ccba776f5e
GitHub Action <action@github.com>
2022-04-24 09:03:36 +0000

    update table data

--- scripts/f3standingData.js
+++ scripts/f3standingData.js
@@ -10,121 +10,121 @@ var tabledata=`<table border="1" class="dataframe">
       <td>2</td>
       <td>A. Leclerc</td>
       <td>PREMA Racing</td>
-      <td>24</td>
+      <td>34</td>
     </tr>
     <tr>
       <td>3</td>
-      <td>F. Colapinto</td>
-      <td>Van Amersfoort Racing</td>
-      <td>22</td>
+      <td>R. Stanek</td>
+      <td>Trident</td>
+      <td>33</td>
     </tr>
     <tr>
       <td>4</td>
-      <td>O. Bearman</td>
+      <td>J. Crawford</td>
       <td>PREMA Racing</td>
-      <td>17</td>
+      <td>32</td>
     </tr>
     <tr>
       <td>5</td>
       <td>I. Hadjar</td>
       <td>Hitech Grand Prix</td>
-      <td>16</td>
+      <td>31</td>
     </tr>
     <tr>
       <td>6</td>
+      <td>O. Bearman</td>
+      <td>PREMA Racing</td>
+      <td>29</td>
+    </tr>
+    <tr>
+      <td>7</td>
+      <td>F. Colapinto</td>
+      <td>Van Amersfoort Racing</td>
+      <td>22</td>
+    </tr>
+    <tr>
+      <td>8</td>
       <td>G. Saucy</td>
       <td>ART Grand Prix</td>
       <td>15</td>
     </tr>
     <tr>
-      <td>7</td>
-      <td>J. Crawford</td>
-      <td>PREMA Racing</td>
-      <td>14</td>
-    </tr>
-    <tr>
-      <td>8</td>
+      <td>9</td>
       <td>J. Correa</td>
       <td>ART Grand Prix</td>
       <td>14</td>
     </tr>
     <tr>
-      <td>9</td>
+      <td>10</td>
       <td>Z. Maloney</td>
       <td>Trident</td>
       <td>13</td>
     </tr>
     <tr>
-      <td>10</td>
+      <td>11</td>
       <td>A. Smolyar</td>
       <td>MP Motorsport</td>
       <td>11</td>
     </tr>
     <tr>
-      <td>11</td>
-      <td>R. Stanek</td>
-      <td>Trident</td>
-      <td>7</td>
-    </tr>
-    <tr>
       <td>12</td>
       <td>Z. O'Sullivan</td>
       <td>Carlin</td>
-      <td>5</td>
+      <td>11</td>
     </tr>
     <tr>
       <td>13</td>
-      <td>D. Vidales</td>
-      <td>Campos Racing</td>
-      <td>5</td>
+      <td>K. Maini</td>
+      <td>MP Motorsport</td>
+      <td>8</td>
     </tr>
     <tr>
       <td>14</td>
+      <td>K. Frederick</td>
+      <td>Hitech Grand Prix</td>
+      <td>8</td>
+    </tr>
+    <tr>
+      <td>15</td>
+      <td>D. Vidales</td>
+      <td>Campos Racing</td>
+      <td>7</td>
+    </tr>
+    <tr>
+      <td>16</td>
       <td>C. Collet</td>
       <td>MP Motorsport</td>
       <td>4</td>
     </tr>
     <tr>
-      <td>15</td>
+      <td>17</td>
       <td>O. Rasmussen</td>
       <td>Trident</td>
       <td>4</td>
     </tr>
     <tr>
-      <td>16</td>
-      <td>K. Frederick</td>
-      <td>Hitech Grand Prix</td>
-      <td>4</td>
-    </tr>
-    <tr>
-      <td>17</td>
+      <td>18</td>
       <td>W. Alatalo</td>
       <td>Jenzer Motorsport</td>
       <td>2</td>
     </tr>
     <tr>
-      <td>18</td>
+      <td>19</td>
       <td>R. Villagómez</td>
       <td>Van Amersfoort Racing</td>
       <td>2</td>
     </tr>
     <tr>
-      <td>19</td>
-      <td>F. Pizzi</td>
-      <td>Charouz Racing System</td>
+      <td>20</td>
+      <td>I. Cohen</td>
+      <td>Jenzer Motorsport</td>
       <td>1</td>
     </tr>
     <tr>
-      <td>20</td>
-      <td>K. Maini</td>
-      <td>MP Motorsport</td>
-      <td>0</td>
-    </tr>
-    <tr>
       <td>21</td>
-      <td>I. Cohen</td>
-      <td>Jenzer Motorsport</td>
-      <td>0</td>
+      <td>F. Pizzi</td>
+      <td>Charouz Racing System</td>
+      <td>1</td>
     </tr>
     <tr>
       <td>23</td>
@@ -188,48 +188,48 @@ var tabledata=`<table border="1" class="dataframe">
   <tbody>
     <tr>
       <td>1</td>
+      <td>PREMA Racing</td>
+      <td>95</td>
+    </tr>
+    <tr>
+      <td>2</td>
       <td>ART Grand Prix</td>
       <td>63</td>
     </tr>
     <tr>
-      <td>2</td>
-      <td>PREMA Racing</td>
-      <td>55</td>
-    </tr>
-    <tr>
       <td>3</td>
       <td>Trident</td>
-      <td>24</td>
+      <td>50</td>
     </tr>
     <tr>
       <td>4</td>
+      <td>Hitech Grand Prix</td>
+      <td>39</td>
+    </tr>
+    <tr>
+      <td>5</td>
       <td>Van Amersfoort Racing</td>
       <td>24</td>
     </tr>
     <tr>
-      <td>5</td>
-      <td>Hitech Grand Prix</td>
-      <td>20</td>
-    </tr>
-    <tr>
       <td>6</td>
       <td>MP Motorsport</td>
-      <td>15</td>
+      <td>23</td>
     </tr>
     <tr>
       <td>7</td>
       <td>Carlin</td>
-      <td>5</td>
+      <td>11</td>
     </tr>
     <tr>
       <td>8</td>
       <td>Campos Racing</td>
-      <td>5</td>
+      <td>7</td>
     </tr>
     <tr>
       <td>9</td>
       <td>Jenzer Motorsport</td>
-      <td>2</td>
+      <td>3</td>
     </tr>
     <tr>
       <td>10</td>
