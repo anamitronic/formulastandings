3cd15234e72798c0b4f38607c7cf90ccb939d8a4
GitHub Action <action@github.com>
2022-05-29 16:53:04 +0000

    update table data

--- scripts/f1standingData.js
+++ scripts/f1standingData.js
@@ -4,49 +4,49 @@ var tabledata=`<table border="1" class="dataframe">
       <td>1</td>
       <td>Max Verstappen VER</td>
       <td>Red Bull Racing RBPT</td>
-      <td>110</td>
+      <td>125</td>
     </tr>
     <tr>
       <td>2</td>
       <td>Charles Leclerc LEC</td>
       <td>Ferrari</td>
-      <td>104</td>
+      <td>116</td>
     </tr>
     <tr>
       <td>3</td>
       <td>Sergio Perez PER</td>
       <td>Red Bull Racing RBPT</td>
-      <td>85</td>
+      <td>110</td>
     </tr>
     <tr>
       <td>4</td>
       <td>George Russell RUS</td>
       <td>Mercedes</td>
-      <td>74</td>
+      <td>84</td>
     </tr>
     <tr>
       <td>5</td>
       <td>Carlos Sainz SAI</td>
       <td>Ferrari</td>
-      <td>65</td>
+      <td>83</td>
     </tr>
     <tr>
       <td>6</td>
       <td>Lewis Hamilton HAM</td>
       <td>Mercedes</td>
-      <td>46</td>
+      <td>50</td>
     </tr>
     <tr>
       <td>7</td>
       <td>Lando Norris NOR</td>
       <td>McLaren Mercedes</td>
-      <td>39</td>
+      <td>48</td>
     </tr>
     <tr>
       <td>8</td>
       <td>Valtteri Bottas BOT</td>
       <td>Alfa Romeo Ferrari</td>
-      <td>38</td>
+      <td>40</td>
     </tr>
     <tr>
       <td>9</td>
@@ -62,33 +62,33 @@ var tabledata=`<table border="1" class="dataframe">
     </tr>
     <tr>
       <td>11</td>
-      <td>Yuki Tsunoda TSU</td>
-      <td>AlphaTauri RBPT</td>
-      <td>11</td>
-    </tr>
-    <tr>
-      <td>12</td>
       <td>Daniel Ricciardo RIC</td>
       <td>McLaren Mercedes</td>
       <td>11</td>
     </tr>
     <tr>
+      <td>12</td>
+      <td>Yuki Tsunoda TSU</td>
+      <td>AlphaTauri RBPT</td>
+      <td>11</td>
+    </tr>
+    <tr>
       <td>13</td>
+      <td>Fernando Alonso ALO</td>
+      <td>Alpine Renault</td>
+      <td>10</td>
+    </tr>
+    <tr>
+      <td>14</td>
       <td>Pierre Gasly GAS</td>
       <td>AlphaTauri RBPT</td>
       <td>6</td>
     </tr>
     <tr>
-      <td>14</td>
+      <td>15</td>
       <td>Sebastian Vettel VET</td>
       <td>Aston Martin Aramco Mercedes</td>
-      <td>4</td>
-    </tr>
-    <tr>
-      <td>15</td>
-      <td>Fernando Alonso ALO</td>
-      <td>Alpine Renault</td>
-      <td>4</td>
+      <td>5</td>
     </tr>
     <tr>
       <td>16</td>
@@ -135,32 +135,32 @@ var tabledata=`<table border="1" class="dataframe">
     <tr>
       <td>1</td>
       <td>Red Bull Racing RBPT</td>
-      <td>195</td>
+      <td>235</td>
     </tr>
     <tr>
       <td>2</td>
       <td>Ferrari</td>
-      <td>169</td>
+      <td>199</td>
     </tr>
     <tr>
       <td>3</td>
       <td>Mercedes</td>
-      <td>120</td>
+      <td>134</td>
     </tr>
     <tr>
       <td>4</td>
       <td>McLaren Mercedes</td>
-      <td>50</td>
+      <td>59</td>
     </tr>
     <tr>
       <td>5</td>
       <td>Alfa Romeo Ferrari</td>
-      <td>39</td>
+      <td>41</td>
     </tr>
     <tr>
       <td>6</td>
       <td>Alpine Renault</td>
-      <td>34</td>
+      <td>40</td>
     </tr>
     <tr>
       <td>7</td>
@@ -175,7 +175,7 @@ var tabledata=`<table border="1" class="dataframe">
     <tr>
       <td>9</td>
       <td>Aston Martin Aramco Mercedes</td>
-      <td>6</td>
+      <td>7</td>
     </tr>
     <tr>
       <td>10</td>
@@ -186,4 +186,4 @@ var tabledata=`<table border="1" class="dataframe">
 </table>
 `;
 document.getElementById('f1-constructors').innerHTML+=tabledata;
-document.getElementById('lastup').innerHTML='29/05/2022 16:06:28 UTC';
+document.getElementById('lastup').innerHTML='29/05/2022 16:52:45 UTC';
