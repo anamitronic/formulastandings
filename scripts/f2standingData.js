2c058661c8a2b4789591b3159d8865d5710e57e4
GitHub Action <action@github.com>
2022-06-12 09:03:44 +0000

    update table data

--- scripts/f2standingData.js
+++ scripts/f2standingData.js
@@ -4,7 +4,7 @@ var tabledata=`<table border="1" class="dataframe">
       <td>1</td>
       <td>F. Drugovich</td>
       <td>MP Motorsport</td>
-      <td>117</td>
+      <td>132</td>
     </tr>
     <tr>
       <td>2</td>
@@ -16,67 +16,67 @@ var tabledata=`<table border="1" class="dataframe">
       <td>3</td>
       <td>J. Daruvala</td>
       <td>PREMA Racing</td>
-      <td>61</td>
+      <td>73</td>
     </tr>
     <tr>
       <td>4</td>
+      <td>L. Sargeant</td>
+      <td>Carlin</td>
+      <td>59</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>D. Hauger</td>
+      <td>PREMA Racing</td>
+      <td>55</td>
+    </tr>
+    <tr>
+      <td>6</td>
       <td>M. Armstrong</td>
       <td>Hitech Grand Prix</td>
       <td>55</td>
     </tr>
     <tr>
-      <td>5</td>
+      <td>7</td>
       <td>J. Vips</td>
       <td>Hitech Grand Prix</td>
-      <td>49</td>
-    </tr>
-    <tr>
-      <td>6</td>
-      <td>J. Doohan</td>
-      <td>Virtuosi Racing</td>
-      <td>45</td>
-    </tr>
-    <tr>
-      <td>7</td>
-      <td>L. Lawson</td>
-      <td>Carlin</td>
-      <td>44</td>
+      <td>51</td>
     </tr>
     <tr>
       <td>8</td>
       <td>E. Fittipaldi</td>
       <td>Charouz Racing System</td>
-      <td>42</td>
+      <td>50</td>
     </tr>
     <tr>
       <td>9</td>
-      <td>L. Sargeant</td>
-      <td>Carlin</td>
-      <td>41</td>
+      <td>J. Doohan</td>
+      <td>Virtuosi Racing</td>
+      <td>45</td>
     </tr>
     <tr>
       <td>10</td>
-      <td>F. Vesti</td>
-      <td>ART Grand Prix</td>
-      <td>35</td>
+      <td>L. Lawson</td>
+      <td>Carlin</td>
+      <td>44</td>
     </tr>
     <tr>
       <td>11</td>
       <td>R. Verschoor</td>
       <td>Trident</td>
-      <td>32</td>
+      <td>42</td>
     </tr>
     <tr>
       <td>12</td>
-      <td>R. Boschung</td>
-      <td>Campos Racing</td>
-      <td>32</td>
+      <td>F. Vesti</td>
+      <td>ART Grand Prix</td>
+      <td>41</td>
     </tr>
     <tr>
       <td>13</td>
-      <td>D. Hauger</td>
-      <td>PREMA Racing</td>
-      <td>30</td>
+      <td>R. Boschung</td>
+      <td>Campos Racing</td>
+      <td>34</td>
     </tr>
     <tr>
       <td>14</td>
@@ -92,27 +92,27 @@ var tabledata=`<table border="1" class="dataframe">
     </tr>
     <tr>
       <td>16</td>
+      <td>J. Hughes</td>
+      <td>Van Amersfoort Racing</td>
+      <td>15</td>
+    </tr>
+    <tr>
+      <td>17</td>
       <td>R. Nissany</td>
       <td>DAMS</td>
       <td>14</td>
     </tr>
     <tr>
-      <td>17</td>
-      <td>J. Hughes</td>
-      <td>Van Amersfoort Racing</td>
-      <td>14</td>
+      <td>18</td>
+      <td>M. Sato</td>
+      <td>Virtuosi Racing</td>
+      <td>6</td>
     </tr>
     <tr>
-      <td>18</td>
+      <td>19</td>
       <td>C. Williams</td>
       <td>Trident</td>
       <td>5</td>
-    </tr>
-    <tr>
-      <td>20</td>
-      <td>M. Sato</td>
-      <td>Virtuosi Racing</td>
-      <td>2</td>
     </tr>
     <tr>
       <td>21</td>
@@ -141,57 +141,57 @@ var tabledata=`<table border="1" class="dataframe">
     <tr>
       <td>1</td>
       <td>MP Motorsport</td>
-      <td>139</td>
+      <td>154</td>
     </tr>
     <tr>
       <td>2</td>
-      <td>ART Grand Prix</td>
-      <td>118</td>
+      <td>PREMA Racing</td>
+      <td>128</td>
     </tr>
     <tr>
       <td>3</td>
-      <td>Hitech Grand Prix</td>
-      <td>104</td>
+      <td>ART Grand Prix</td>
+      <td>124</td>
     </tr>
     <tr>
       <td>4</td>
-      <td>PREMA Racing</td>
-      <td>91</td>
+      <td>Hitech Grand Prix</td>
+      <td>106</td>
     </tr>
     <tr>
       <td>5</td>
       <td>Carlin</td>
-      <td>85</td>
+      <td>103</td>
     </tr>
     <tr>
       <td>6</td>
-      <td>Virtuosi Racing</td>
-      <td>47</td>
+      <td>Charouz Racing System</td>
+      <td>54</td>
     </tr>
     <tr>
       <td>7</td>
-      <td>Charouz Racing System</td>
-      <td>46</td>
+      <td>Virtuosi Racing</td>
+      <td>51</td>
     </tr>
     <tr>
       <td>8</td>
+      <td>Trident</td>
+      <td>47</td>
+    </tr>
+    <tr>
+      <td>9</td>
       <td>DAMS</td>
       <td>43</td>
     </tr>
     <tr>
-      <td>9</td>
-      <td>Trident</td>
-      <td>37</td>
-    </tr>
-    <tr>
       <td>10</td>
       <td>Campos Racing</td>
-      <td>32</td>
+      <td>34</td>
     </tr>
     <tr>
       <td>11</td>
       <td>Van Amersfoort Racing</td>
-      <td>14</td>
+      <td>15</td>
     </tr>
   </tbody>
 </table>
