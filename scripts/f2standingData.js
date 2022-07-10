b527498018ec9d42d846b12f98c0f5d2143f49b5
GitHub Action <action@github.com>
2022-07-10 14:02:32 +0000

    update table data

--- scripts/f2standingData.js
+++ scripts/f2standingData.js
@@ -8,27 +8,27 @@ var tabledata=`<table border="1" class="dataframe">
     </tr>
     <tr>
       <td>2</td>
+      <td>L. Sargeant</td>
+      <td>Carlin</td>
+      <td>115</td>
+    </tr>
+    <tr>
+      <td>3</td>
       <td>T. Pourchaire</td>
       <td>ART Grand Prix</td>
       <td>114</td>
     </tr>
     <tr>
-      <td>3</td>
-      <td>L. Sargeant</td>
-      <td>Carlin</td>
-      <td>105</td>
-    </tr>
-    <tr>
       <td>4</td>
       <td>J. Daruvala</td>
       <td>PREMA Racing</td>
-      <td>98</td>
+      <td>80</td>
     </tr>
     <tr>
       <td>5</td>
-      <td>R. Verschoor</td>
-      <td>Trident</td>
-      <td>70</td>
+      <td>E. Fittipaldi</td>
+      <td>Charouz Racing System</td>
+      <td>75</td>
     </tr>
     <tr>
       <td>6</td>
@@ -38,87 +38,87 @@ var tabledata=`<table border="1" class="dataframe">
     </tr>
     <tr>
       <td>7</td>
-      <td>E. Fittipaldi</td>
-      <td>Charouz Racing System</td>
-      <td>69</td>
+      <td>D. Hauger</td>
+      <td>PREMA Racing</td>
+      <td>67</td>
     </tr>
     <tr>
       <td>8</td>
       <td>J. Vips</td>
       <td>Hitech Grand Prix</td>
-      <td>64</td>
+      <td>67</td>
     </tr>
     <tr>
       <td>9</td>
-      <td>D. Hauger</td>
-      <td>PREMA Racing</td>
-      <td>63</td>
-    </tr>
-    <tr>
-      <td>10</td>
       <td>J. Doohan</td>
       <td>Virtuosi Racing</td>
       <td>63</td>
     </tr>
     <tr>
-      <td>11</td>
+      <td>10</td>
       <td>L. Lawson</td>
       <td>Carlin</td>
-      <td>59</td>
+      <td>60</td>
     </tr>
     <tr>
-      <td>12</td>
+      <td>11</td>
       <td>F. Vesti</td>
       <td>ART Grand Prix</td>
       <td>56</td>
     </tr>
     <tr>
+      <td>12</td>
+      <td>R. Verschoor</td>
+      <td>Trident</td>
+      <td>45</td>
+    </tr>
+    <tr>
       <td>13</td>
       <td>A. Iwasa</td>
       <td>DAMS</td>
-      <td>40</td>
+      <td>44</td>
     </tr>
     <tr>
       <td>15</td>
-      <td>C. Novalak</td>
-      <td>MP Motorsport</td>
-      <td>22</td>
+      <td>J. Hughes</td>
+      <td>Van Amersfoort Racing</td>
+      <td>26</td>
     </tr>
     <tr>
       <td>16</td>
-      <td>J. Hughes</td>
-      <td>Van Amersfoort Racing</td>
+      <td>C. Novalak</td>
+      <td>MP Motorsport</td>
       <td>22</td>
     </tr>
     <tr>
       <td>17</td>
       <td>R. Nissany</td>
       <td>DAMS</td>
-      <td>14</td>
+      <td>16</td>
     </tr>
     <tr>
       <td>18</td>
       <td>R. Merhi</td>
       <td>Campos Racing</td>
-      <td>10</td>
+      <td>15</td>
     </tr>
     <tr>
       <td>19</td>
+      <td>O. Caldwell</td>
+      <td>Campos Racing</td>
+      <td>8</td>
+    </tr>
+    <tr>
+      <td>20</td>
       <td>M. Sato</td>
       <td>Virtuosi Racing</td>
       <td>6</td>
     </tr>
     <tr>
-      <td>20</td>
+      <td>21</td>
       <td>C. Williams</td>
       <td>Trident</td>
       <td>5</td>
-    </tr>
-    <tr>
-      <td>21</td>
-      <td>O. Caldwell</td>
-      <td>Campos Racing</td>
-      <td>4</td>
     </tr>
     <tr>
       <td>23</td>
@@ -145,53 +145,53 @@ var tabledata=`<table border="1" class="dataframe">
     </tr>
     <tr>
       <td>2</td>
+      <td>Carlin</td>
+      <td>175</td>
+    </tr>
+    <tr>
+      <td>3</td>
       <td>ART Grand Prix</td>
       <td>170</td>
     </tr>
     <tr>
-      <td>3</td>
-      <td>Carlin</td>
-      <td>164</td>
-    </tr>
-    <tr>
       <td>4</td>
       <td>PREMA Racing</td>
-      <td>161</td>
+      <td>147</td>
     </tr>
     <tr>
       <td>5</td>
       <td>Hitech Grand Prix</td>
-      <td>133</td>
+      <td>136</td>
     </tr>
     <tr>
       <td>6</td>
-      <td>Trident</td>
-      <td>75</td>
+      <td>Charouz Racing System</td>
+      <td>79</td>
     </tr>
     <tr>
       <td>7</td>
-      <td>Charouz Racing System</td>
-      <td>73</td>
-    </tr>
-    <tr>
-      <td>8</td>
       <td>Virtuosi Racing</td>
       <td>69</td>
     </tr>
     <tr>
-      <td>9</td>
+      <td>8</td>
       <td>DAMS</td>
-      <td>54</td>
+      <td>60</td>
+    </tr>
+    <tr>
+      <td>9</td>
+      <td>Campos Racing</td>
+      <td>57</td>
     </tr>
     <tr>
       <td>10</td>
-      <td>Campos Racing</td>
-      <td>48</td>
+      <td>Trident</td>
+      <td>50</td>
     </tr>
     <tr>
       <td>11</td>
       <td>Van Amersfoort Racing</td>
-      <td>22</td>
+      <td>26</td>
     </tr>
   </tbody>
 </table>
