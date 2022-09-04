3fecfe7ff1c911ae5919caf1a7e01080a588ab51
GitHub Action <action@github.com>
2022-09-04 10:39:01 +0000

    update table data

--- scripts/f2standingData.js
+++ scripts/f2standingData.js
@@ -4,13 +4,13 @@ var tabledata=`<table border="1" class="dataframe">
       <td>1</td>
       <td>F. Drugovich</td>
       <td>MP Motorsport</td>
-      <td>206</td>
+      <td>231</td>
     </tr>
     <tr>
       <td>2</td>
       <td>T. Pourchaire</td>
       <td>ART Grand Prix</td>
-      <td>162</td>
+      <td>163</td>
     </tr>
     <tr>
       <td>3</td>
@@ -32,27 +32,27 @@ var tabledata=`<table border="1" class="dataframe">
     </tr>
     <tr>
       <td>6</td>
-      <td>E. Fittipaldi</td>
-      <td>Charouz Racing System</td>
-      <td>101</td>
+      <td>A. Iwasa</td>
+      <td>DAMS</td>
+      <td>114</td>
     </tr>
     <tr>
       <td>7</td>
-      <td>A. Iwasa</td>
-      <td>DAMS</td>
-      <td>99</td>
+      <td>E. Fittipaldi</td>
+      <td>Charouz Racing System</td>
+      <td>111</td>
     </tr>
     <tr>
       <td>8</td>
-      <td>J. Daruvala</td>
-      <td>PREMA Racing</td>
-      <td>94</td>
+      <td>J. Vips</td>
+      <td>Hitech Grand Prix</td>
+      <td>99</td>
     </tr>
     <tr>
       <td>9</td>
-      <td>J. Vips</td>
-      <td>Hitech Grand Prix</td>
-      <td>93</td>
+      <td>J. Daruvala</td>
+      <td>PREMA Racing</td>
+      <td>94</td>
     </tr>
     <tr>
       <td>10</td>
@@ -70,13 +70,13 @@ var tabledata=`<table border="1" class="dataframe">
       <td>12</td>
       <td>D. Hauger</td>
       <td>PREMA Racing</td>
-      <td>73</td>
+      <td>85</td>
     </tr>
     <tr>
       <td>13</td>
       <td>R. Verschoor</td>
       <td>Trident</td>
-      <td>67</td>
+      <td>85</td>
     </tr>
     <tr>
       <td>14</td>
@@ -97,34 +97,34 @@ var tabledata=`<table border="1" class="dataframe">
       <td>18</td>
     </tr>
     <tr>
-      <td>19</td>
+      <td>18</td>
       <td>D. Beckmann</td>
       <td>Van Amersfoort Racing</td>
-      <td>13</td>
+      <td>17</td>
     </tr>
     <tr>
       <td>20</td>
       <td>O. Caldwell</td>
       <td>Campos Racing</td>
-      <td>8</td>
+      <td>10</td>
     </tr>
     <tr>
       <td>21</td>
+      <td>A. Cordeel</td>
+      <td>Van Amersfoort Racing</td>
+      <td>8</td>
+    </tr>
+    <tr>
+      <td>22</td>
       <td>M. Sato</td>
       <td>Virtuosi Racing</td>
       <td>6</td>
     </tr>
     <tr>
-      <td>22</td>
+      <td>23</td>
       <td>C. Williams</td>
       <td>Trident</td>
       <td>5</td>
-    </tr>
-    <tr>
-      <td>23</td>
-      <td>A. Cordeel</td>
-      <td>Van Amersfoort Racing</td>
-      <td>0</td>
     </tr>
     <tr>
       <td>25</td>
@@ -140,58 +140,58 @@ var tabledata=`<table border="1" class="dataframe">
   <tbody>
     <tr>
       <td>1</td>
-      <td>ART Grand Prix</td>
-      <td>253</td>
+      <td>MP Motorsport</td>
+      <td>267</td>
     </tr>
     <tr>
       <td>2</td>
+      <td>ART Grand Prix</td>
+      <td>254</td>
+    </tr>
+    <tr>
+      <td>3</td>
       <td>Carlin</td>
       <td>249</td>
     </tr>
     <tr>
-      <td>3</td>
-      <td>MP Motorsport</td>
-      <td>242</td>
-    </tr>
-    <tr>
       <td>4</td>
       <td>Hitech Grand Prix</td>
-      <td>184</td>
+      <td>190</td>
     </tr>
     <tr>
       <td>5</td>
       <td>PREMA Racing</td>
-      <td>167</td>
+      <td>179</td>
     </tr>
     <tr>
       <td>6</td>
+      <td>DAMS</td>
+      <td>132</td>
+    </tr>
+    <tr>
+      <td>7</td>
       <td>Virtuosi Racing</td>
       <td>127</td>
     </tr>
     <tr>
-      <td>7</td>
-      <td>DAMS</td>
-      <td>117</td>
-    </tr>
-    <tr>
       <td>8</td>
       <td>Charouz Racing System</td>
-      <td>105</td>
+      <td>115</td>
     </tr>
     <tr>
       <td>9</td>
       <td>Trident</td>
-      <td>72</td>
+      <td>90</td>
     </tr>
     <tr>
       <td>10</td>
       <td>Campos Racing</td>
-      <td>63</td>
+      <td>65</td>
     </tr>
     <tr>
       <td>11</td>
       <td>Van Amersfoort Racing</td>
-      <td>35</td>
+      <td>47</td>
     </tr>
   </tbody>
 </table>
