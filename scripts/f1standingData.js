ef03e26a7031f397c953f9fd184104cb36c42e7a
GitHub Action <action@github.com>
2023-03-05 17:02:25 +0000

    update table data

--- scripts/f1standingData.js
+++ scripts/f1standingData.js
@@ -1,11 +1,165 @@
-var tabledata=`<tbody>
-	<td colspan='100' style='text-align:center'>The 2023 season is not currently in session</td>
-</tbody>
+var tabledata=`<table border="1" class="dataframe">
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Max Verstappen VER</td>
+      <td>Red Bull Racing Honda RBPT</td>
+      <td>25</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Sergio Perez PER</td>
+      <td>Red Bull Racing Honda RBPT</td>
+      <td>18</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Fernando Alonso ALO</td>
+      <td>Aston Martin Aramco Mercedes</td>
+      <td>15</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>Carlos Sainz SAI</td>
+      <td>Ferrari</td>
+      <td>12</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>Lewis Hamilton HAM</td>
+      <td>Mercedes</td>
+      <td>10</td>
+    </tr>
+    <tr>
+      <td>6</td>
+      <td>Lance Stroll STR</td>
+      <td>Aston Martin Aramco Mercedes</td>
+      <td>8</td>
+    </tr>
+    <tr>
+      <td>7</td>
+      <td>George Russell RUS</td>
+      <td>Mercedes</td>
+      <td>6</td>
+    </tr>
+    <tr>
+      <td>8</td>
+      <td>Valtteri Bottas BOT</td>
+      <td>Alfa Romeo Ferrari</td>
+      <td>4</td>
+    </tr>
+    <tr>
+      <td>9</td>
+      <td>Pierre Gasly GAS</td>
+      <td>Alpine Renault</td>
+      <td>2</td>
+    </tr>
+    <tr>
+      <td>10</td>
+      <td>Alexander Albon ALB</td>
+      <td>Williams Mercedes</td>
+      <td>1</td>
+    </tr>
+    <tr>
+      <td>11</td>
+      <td>Yuki Tsunoda TSU</td>
+      <td>AlphaTauri Honda RBPT</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>12</td>
+      <td>Logan Sargeant SAR</td>
+      <td>Williams Mercedes</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>13</td>
+      <td>Kevin Magnussen MAG</td>
+      <td>Haas Ferrari</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>14</td>
+      <td>Nyck De Vries DEV</td>
+      <td>AlphaTauri Honda RBPT</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>15</td>
+      <td>Nico Hulkenberg HUL</td>
+      <td>Haas Ferrari</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>16</td>
+      <td>Zhou Guanyu ZHO</td>
+      <td>Alfa Romeo Ferrari</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>17</td>
+      <td>Lando Norris NOR</td>
+      <td>McLaren Mercedes</td>
+      <td>0</td>
+    </tr>
+  </tbody>
+</table>
 `;
 document.getElementById('f1-drivers').innerHTML+=tabledata;
-var tabledata=`<tbody>
-	<td colspan='100' style='text-align:center'>The 2023 season is not currently in session</td>
-</tbody>
+var tabledata=`<table border="1" class="dataframe">
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Red Bull Racing Honda RBPT</td>
+      <td>43</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Aston Martin Aramco Mercedes</td>
+      <td>23</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Mercedes</td>
+      <td>16</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>Ferrari</td>
+      <td>12</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>Alfa Romeo Ferrari</td>
+      <td>4</td>
+    </tr>
+    <tr>
+      <td>6</td>
+      <td>Alpine Renault</td>
+      <td>2</td>
+    </tr>
+    <tr>
+      <td>7</td>
+      <td>Williams Mercedes</td>
+      <td>1</td>
+    </tr>
+    <tr>
+      <td>8</td>
+      <td>AlphaTauri Honda RBPT</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>9</td>
+      <td>Haas Ferrari</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>10</td>
+      <td>McLaren Mercedes</td>
+      <td>0</td>
+    </tr>
+  </tbody>
+</table>
 `;
 document.getElementById('f1-constructors').innerHTML+=tabledata;
-document.getElementById('lastup').innerHTML='05/03/2023 16:40:25 UTC';
+document.getElementById('lastup').innerHTML='05/03/2023 17:02:13 UTC';
