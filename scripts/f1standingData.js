931233cd479bbd8f7e81d219a437794a5ad7751e
GitHub Action <action@github.com>
2023-03-19 18:33:35 +0000

    update table data

--- scripts/f1standingData.js
+++ scripts/f1standingData.js
@@ -4,83 +4,83 @@ var tabledata=`<table border="1" class="dataframe">
       <td>1</td>
       <td>Max Verstappen VER</td>
       <td>Red Bull Racing Honda RBPT</td>
-      <td>25</td>
+      <td>44</td>
     </tr>
     <tr>
       <td>2</td>
       <td>Sergio Perez PER</td>
       <td>Red Bull Racing Honda RBPT</td>
-      <td>18</td>
+      <td>43</td>
     </tr>
     <tr>
       <td>3</td>
       <td>Fernando Alonso ALO</td>
       <td>Aston Martin Aramco Mercedes</td>
-      <td>15</td>
+      <td>30</td>
     </tr>
     <tr>
       <td>4</td>
       <td>Carlos Sainz SAI</td>
       <td>Ferrari</td>
-      <td>12</td>
+      <td>20</td>
     </tr>
     <tr>
       <td>5</td>
       <td>Lewis Hamilton HAM</td>
       <td>Mercedes</td>
-      <td>10</td>
+      <td>20</td>
     </tr>
     <tr>
       <td>6</td>
+      <td>George Russell RUS</td>
+      <td>Mercedes</td>
+      <td>18</td>
+    </tr>
+    <tr>
+      <td>7</td>
       <td>Lance Stroll STR</td>
       <td>Aston Martin Aramco Mercedes</td>
       <td>8</td>
     </tr>
     <tr>
-      <td>7</td>
-      <td>George Russell RUS</td>
-      <td>Mercedes</td>
-      <td>6</td>
-    </tr>
-    <tr>
       <td>8</td>
-      <td>Valtteri Bottas BOT</td>
-      <td>Alfa Romeo Ferrari</td>
-      <td>4</td>
+      <td>Charles Leclerc LEC</td>
+      <td>Ferrari</td>
+      <td>6</td>
     </tr>
     <tr>
       <td>9</td>
       <td>Pierre Gasly GAS</td>
       <td>Alpine Renault</td>
-      <td>2</td>
+      <td>4</td>
     </tr>
     <tr>
       <td>10</td>
+      <td>Esteban Ocon OCO</td>
+      <td>Alpine Renault</td>
+      <td>4</td>
+    </tr>
+    <tr>
+      <td>11</td>
+      <td>Valtteri Bottas BOT</td>
+      <td>Alfa Romeo Ferrari</td>
+      <td>4</td>
+    </tr>
+    <tr>
+      <td>12</td>
+      <td>Kevin Magnussen MAG</td>
+      <td>Haas Ferrari</td>
+      <td>1</td>
+    </tr>
+    <tr>
+      <td>13</td>
       <td>Alexander Albon ALB</td>
       <td>Williams Mercedes</td>
       <td>1</td>
     </tr>
     <tr>
-      <td>11</td>
-      <td>Yuki Tsunoda TSU</td>
-      <td>AlphaTauri Honda RBPT</td>
-      <td>0</td>
-    </tr>
-    <tr>
-      <td>12</td>
-      <td>Logan Sargeant SAR</td>
-      <td>Williams Mercedes</td>
-      <td>0</td>
-    </tr>
-    <tr>
-      <td>13</td>
-      <td>Kevin Magnussen MAG</td>
-      <td>Haas Ferrari</td>
-      <td>0</td>
-    </tr>
-    <tr>
       <td>14</td>
-      <td>Nyck De Vries DEV</td>
+      <td>Yuki Tsunoda TSU</td>
       <td>AlphaTauri Honda RBPT</td>
       <td>0</td>
     </tr>
@@ -92,12 +92,30 @@ var tabledata=`<table border="1" class="dataframe">
     </tr>
     <tr>
       <td>16</td>
+      <td>Logan Sargeant SAR</td>
+      <td>Williams Mercedes</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>17</td>
       <td>Zhou Guanyu ZHO</td>
       <td>Alfa Romeo Ferrari</td>
       <td>0</td>
     </tr>
     <tr>
-      <td>17</td>
+      <td>18</td>
+      <td>Nyck De Vries DEV</td>
+      <td>AlphaTauri Honda RBPT</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>19</td>
+      <td>Oscar Piastri PIA</td>
+      <td>McLaren Mercedes</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>20</td>
       <td>Lando Norris NOR</td>
       <td>McLaren Mercedes</td>
       <td>0</td>
@@ -162,4 +180,4 @@ var tabledata=`<table border="1" class="dataframe">
 </table>
 `;
 document.getElementById('f1-constructors').innerHTML+=tabledata;
-document.getElementById('lastup').innerHTML='19/03/2023 18:05:58 UTC';
+document.getElementById('lastup').innerHTML='19/03/2023 18:33:24 UTC';
