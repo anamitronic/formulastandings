bb3f9e1b34078704fc1cdcf8c7e881cb9e7cb931
GitHub Action <action@github.com>
2023-03-19 15:02:33 +0000

    update table data

--- scripts/f2standingData.js
+++ scripts/f2standingData.js
@@ -16,73 +16,73 @@ var tabledata=`<table border="1" class="dataframe">
       <td>3</td>
       <td>A. Iwasa</td>
       <td>DAMS</td>
-      <td>19</td>
+      <td>31</td>
     </tr>
     <tr>
       <td>4</td>
+      <td>F. Vesti</td>
+      <td>PREMA Racing</td>
+      <td>28</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>J. Daruvala</td>
+      <td>MP Motorsport</td>
+      <td>24</td>
+    </tr>
+    <tr>
+      <td>6</td>
+      <td>J. Doohan</td>
+      <td>Invicta Virtuosi Racing</td>
+      <td>20</td>
+    </tr>
+    <tr>
+      <td>7</td>
+      <td>D. Hauger</td>
+      <td>MP Motorsport</td>
+      <td>19</td>
+    </tr>
+    <tr>
+      <td>8</td>
+      <td>R. Verschoor</td>
+      <td>Van Amersfoort Racing</td>
+      <td>19</td>
+    </tr>
+    <tr>
+      <td>9</td>
       <td>K. Maini</td>
       <td>Campos Racing</td>
       <td>18</td>
     </tr>
     <tr>
-      <td>5</td>
+      <td>10</td>
       <td>V. Martins</td>
       <td>ART Grand Prix</td>
-      <td>15</td>
+      <td>17</td>
     </tr>
     <tr>
-      <td>6</td>
+      <td>11</td>
       <td>Z. Maloney</td>
       <td>Rodin Carlin</td>
       <td>15</td>
     </tr>
     <tr>
-      <td>7</td>
-      <td>R. Verschoor</td>
-      <td>Van Amersfoort Racing</td>
-      <td>11</td>
-    </tr>
-    <tr>
-      <td>8</td>
-      <td>D. Hauger</td>
-      <td>MP Motorsport</td>
-      <td>9</td>
-    </tr>
-    <tr>
-      <td>9</td>
-      <td>J. Daruvala</td>
-      <td>MP Motorsport</td>
-      <td>9</td>
-    </tr>
-    <tr>
-      <td>10</td>
+      <td>12</td>
       <td>A. Leclerc</td>
       <td>DAMS</td>
-      <td>8</td>
-    </tr>
-    <tr>
-      <td>11</td>
-      <td>I. Hadjar</td>
-      <td>Hitech Pulse-Eight</td>
-      <td>6</td>
-    </tr>
-    <tr>
-      <td>12</td>
-      <td>F. Vesti</td>
-      <td>PREMA Racing</td>
-      <td>3</td>
+      <td>13</td>
     </tr>
     <tr>
       <td>13</td>
       <td>E. Fittipaldi</td>
       <td>Rodin Carlin</td>
-      <td>3</td>
+      <td>9</td>
     </tr>
     <tr>
       <td>14</td>
-      <td>J. Doohan</td>
-      <td>Invicta Virtuosi Racing</td>
-      <td>2</td>
+      <td>I. Hadjar</td>
+      <td>Hitech Pulse-Eight</td>
+      <td>8</td>
     </tr>
     <tr>
       <td>15</td>
@@ -92,20 +92,20 @@ var tabledata=`<table border="1" class="dataframe">
     </tr>
     <tr>
       <td>16</td>
+      <td>O. Bearman</td>
+      <td>PREMA Racing</td>
+      <td>1</td>
+    </tr>
+    <tr>
+      <td>17</td>
       <td>J. Crawford</td>
       <td>Hitech Pulse-Eight</td>
       <td>0</td>
     </tr>
     <tr>
-      <td>17</td>
+      <td>18</td>
       <td>R. Nissany</td>
       <td>PHM Racing by Charouz</td>
-      <td>0</td>
-    </tr>
-    <tr>
-      <td>18</td>
-      <td>O. Bearman</td>
-      <td>PREMA Racing</td>
       <td>0</td>
     </tr>
     <tr>
@@ -146,42 +146,42 @@ var tabledata=`<table border="1" class="dataframe">
     <tr>
       <td>2</td>
       <td>ART Grand Prix</td>
-      <td>47</td>
+      <td>49</td>
     </tr>
     <tr>
       <td>3</td>
       <td>DAMS</td>
-      <td>27</td>
+      <td>44</td>
     </tr>
     <tr>
       <td>4</td>
       <td>MP Motorsport</td>
-      <td>18</td>
+      <td>43</td>
     </tr>
     <tr>
       <td>5</td>
-      <td>Rodin Carlin</td>
-      <td>18</td>
+      <td>PREMA Racing</td>
+      <td>29</td>
     </tr>
     <tr>
       <td>6</td>
-      <td>Van Amersfoort Racing</td>
-      <td>12</td>
+      <td>Rodin Carlin</td>
+      <td>24</td>
     </tr>
     <tr>
       <td>7</td>
-      <td>Hitech Pulse-Eight</td>
-      <td>6</td>
+      <td>Invicta Virtuosi Racing</td>
+      <td>20</td>
     </tr>
     <tr>
       <td>8</td>
-      <td>PREMA Racing</td>
-      <td>3</td>
+      <td>Van Amersfoort Racing</td>
+      <td>20</td>
     </tr>
     <tr>
       <td>9</td>
-      <td>Invicta Virtuosi Racing</td>
-      <td>2</td>
+      <td>Hitech Pulse-Eight</td>
+      <td>8</td>
     </tr>
     <tr>
       <td>10</td>
