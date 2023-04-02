fae231dcb66aa5e5e377a96de6efa07c31e993f6
GitHub Action <action@github.com>
2023-04-02 03:31:10 +0000

    update table data

--- scripts/f2standingData.js
+++ scripts/f2standingData.js
@@ -2,69 +2,69 @@ var tabledata=`<table border="1" class="dataframe">
   <tbody>
     <tr>
       <td>1</td>
-      <td>R. Boschung</td>
-      <td>Campos Racing</td>
-      <td>33</td>
+      <td>A. Iwasa</td>
+      <td>DAMS</td>
+      <td>58</td>
     </tr>
     <tr>
       <td>2</td>
       <td>T. Pourchaire</td>
       <td>ART Grand Prix</td>
-      <td>32</td>
+      <td>50</td>
     </tr>
     <tr>
       <td>3</td>
-      <td>A. Iwasa</td>
-      <td>DAMS</td>
-      <td>31</td>
+      <td>F. Vesti</td>
+      <td>PREMA Racing</td>
+      <td>42</td>
     </tr>
     <tr>
       <td>4</td>
-      <td>D. Hauger</td>
-      <td>MP Motorsport</td>
-      <td>30</td>
+      <td>R. Boschung</td>
+      <td>Campos Racing</td>
+      <td>33</td>
     </tr>
     <tr>
       <td>5</td>
-      <td>F. Vesti</td>
-      <td>PREMA Racing</td>
-      <td>29</td>
+      <td>A. Leclerc</td>
+      <td>DAMS</td>
+      <td>33</td>
     </tr>
     <tr>
       <td>6</td>
       <td>J. Daruvala</td>
       <td>MP Motorsport</td>
-      <td>24</td>
+      <td>32</td>
     </tr>
     <tr>
       <td>7</td>
-      <td>K. Maini</td>
-      <td>Campos Racing</td>
-      <td>24</td>
+      <td>D. Hauger</td>
+      <td>MP Motorsport</td>
+      <td>30</td>
     </tr>
     <tr>
       <td>8</td>
-      <td>J. Doohan</td>
-      <td>Invicta Virtuosi Racing</td>
-      <td>20</td>
+      <td>Z. Maloney</td>
+      <td>Rodin Carlin</td>
+      <td>29</td>
     </tr>
     <tr>
       <td>9</td>
-      <td>Z. Maloney</td>
-      <td>Rodin Carlin</td>
-      <td>19</td>
+      <td>K. Maini</td>
+      <td>Campos Racing</td>
+      <td>26</td>
     </tr>
     <tr>
       <td>10</td>
       <td>R. Verschoor</td>
       <td>Van Amersfoort Racing</td>
-      <td>19</td>
+      <td>25</td>
     </tr>
     <tr>
       <td>11</td>
-      <td>A. Leclerc</td>
-      <td>DAMS</td>
-      <td>18</td>
+      <td>J. Doohan</td>
+      <td>Invicta Virtuosi Racing</td>
+      <td>24</td>
     </tr>
     <tr>
       <td>12</td>
@@ -104,14 +104,14 @@ var tabledata=`<table border="1" class="dataframe">
     </tr>
     <tr>
       <td>18</td>
-      <td>R. Nissany</td>
-      <td>PHM Racing by Charouz</td>
-      <td>0</td>
+      <td>R. Stanek</td>
+      <td>Trident</td>
+      <td>1</td>
     </tr>
     <tr>
       <td>19</td>
-      <td>R. Stanek</td>
-      <td>Trident</td>
+      <td>R. Nissany</td>
+      <td>PHM Racing by Charouz</td>
       <td>0</td>
     </tr>
     <tr>
@@ -140,43 +140,43 @@ var tabledata=`<table border="1" class="dataframe">
   <tbody>
     <tr>
       <td>1</td>
-      <td>Campos Racing</td>
-      <td>57</td>
+      <td>DAMS</td>
+      <td>91</td>
     </tr>
     <tr>
       <td>2</td>
-      <td>MP Motorsport</td>
-      <td>54</td>
+      <td>ART Grand Prix</td>
+      <td>67</td>
     </tr>
     <tr>
       <td>3</td>
-      <td>DAMS</td>
-      <td>49</td>
+      <td>MP Motorsport</td>
+      <td>62</td>
     </tr>
     <tr>
       <td>4</td>
-      <td>ART Grand Prix</td>
-      <td>49</td>
+      <td>Campos Racing</td>
+      <td>59</td>
     </tr>
     <tr>
       <td>5</td>
       <td>PREMA Racing</td>
-      <td>32</td>
+      <td>45</td>
     </tr>
     <tr>
       <td>6</td>
       <td>Rodin Carlin</td>
-      <td>28</td>
+      <td>38</td>
     </tr>
     <tr>
       <td>7</td>
-      <td>Invicta Virtuosi Racing</td>
-      <td>20</td>
+      <td>Van Amersfoort Racing</td>
+      <td>26</td>
     </tr>
     <tr>
       <td>8</td>
-      <td>Van Amersfoort Racing</td>
-      <td>20</td>
+      <td>Invicta Virtuosi Racing</td>
+      <td>24</td>
     </tr>
     <tr>
       <td>9</td>
@@ -185,12 +185,12 @@ var tabledata=`<table border="1" class="dataframe">
     </tr>
     <tr>
       <td>10</td>
-      <td>PHM Racing by Charouz</td>
-      <td>0</td>
+      <td>Trident</td>
+      <td>1</td>
     </tr>
     <tr>
       <td>11</td>
-      <td>Trident</td>
+      <td>PHM Racing by Charouz</td>
       <td>0</td>
     </tr>
   </tbody>
