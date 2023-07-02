298e1ccbf5c0c48050fba3503689a2e49c06ec60
GitHub Action <action@github.com>
2023-07-02 16:04:10 +0000

    update table data

--- scripts/f1standingData.js
+++ scripts/f1standingData.js
@@ -180,4 +180,4 @@ var tabledata=`<table border="1" class="dataframe">
 </table>
 `;
 document.getElementById('f1-constructors').innerHTML+=tabledata;
-document.getElementById('lastup').innerHTML='02/07/2023 15:30:55 UTC';
+document.getElementById('lastup').innerHTML='02/07/2023 16:04:02 UTC';
--- scripts/f3standingData.js
+++ scripts/f3standingData.js
@@ -8,20 +8,20 @@ var tabledata=`<table border="1" class="dataframe">
     </tr>
     <tr>
       <td>2</td>
+      <td>J. Martí</td>
+      <td>Campos Racing</td>
+      <td>75</td>
+    </tr>
+    <tr>
+      <td>3</td>
       <td>D. Beganovic</td>
       <td>PREMA Racing</td>
       <td>75</td>
     </tr>
     <tr>
-      <td>3</td>
+      <td>4</td>
       <td>Z. O'Sullivan</td>
       <td>PREMA Racing</td>
-      <td>73</td>
-    </tr>
-    <tr>
-      <td>4</td>
-      <td>J. Martí</td>
-      <td>Campos Racing</td>
       <td>73</td>
     </tr>
     <tr>
@@ -58,7 +58,7 @@ var tabledata=`<table border="1" class="dataframe">
       <td>10</td>
       <td>L. Fornaroli</td>
       <td>Trident</td>
-      <td>36</td>
+      <td>37</td>
     </tr>
     <tr>
       <td>11</td>
@@ -70,7 +70,7 @@ var tabledata=`<table border="1" class="dataframe">
       <td>12</td>
       <td>S. Montoya</td>
       <td>Hitech Pulse-Eight</td>
-      <td>26</td>
+      <td>25</td>
     </tr>
     <tr>
       <td>13</td>
@@ -134,20 +134,20 @@ var tabledata=`<table border="1" class="dataframe">
     </tr>
     <tr>
       <td>23</td>
-      <td>H. Yeany</td>
-      <td>Rodin Carlin</td>
-      <td>0</td>
-    </tr>
-    <tr>
-      <td>24</td>
       <td>O. Gray</td>
       <td>Rodin Carlin</td>
       <td>0</td>
     </tr>
     <tr>
-      <td>25</td>
+      <td>24</td>
       <td>R. Villagómez</td>
       <td>Van Amersfoort Racing</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>25</td>
+      <td>H. Yeany</td>
+      <td>Rodin Carlin</td>
       <td>0</td>
     </tr>
     <tr>
@@ -163,14 +163,14 @@ var tabledata=`<table border="1" class="dataframe">
       <td>0</td>
     </tr>
     <tr>
-      <td>29</td>
-      <td>R. Faria</td>
+      <td>28</td>
+      <td>M. Cresswell</td>
       <td>PHM Racing by Charouz</td>
       <td>0</td>
     </tr>
     <tr>
-      <td>30</td>
-      <td>M. Cresswell</td>
+      <td>29</td>
+      <td>R. Faria</td>
       <td>PHM Racing by Charouz</td>
       <td>0</td>
     </tr>
@@ -194,17 +194,17 @@ var tabledata=`<table border="1" class="dataframe">
     <tr>
       <td>2</td>
       <td>Trident</td>
-      <td>170</td>
+      <td>171</td>
     </tr>
     <tr>
       <td>3</td>
       <td>Hitech Pulse-Eight</td>
-      <td>129</td>
+      <td>128</td>
     </tr>
     <tr>
       <td>4</td>
       <td>Campos Racing</td>
-      <td>88</td>
+      <td>90</td>
     </tr>
     <tr>
       <td>5</td>
