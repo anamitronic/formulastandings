594767cf9c78a749628451b1fefced7a4263067d
GitHub Action <action@github.com>
2024-01-01 00:31:47 +0000

    update table data

--- scripts/festandingData.js
+++ scripts/festandingData.js
@@ -1,10 +1,10 @@
 var tabledata=`<tbody>
-	<td colspan='100' style='text-align:center'>The 2023 season is not currently in session</td>
+	<td colspan='100' style='text-align:center'>The 2024 season is not currently in session</td>
 </tbody>
 `;
 document.getElementById('fe-drivers').innerHTML+=tabledata;
 var tabledata=`<tbody>
-	<td colspan='100' style='text-align:center'>The 2023 season is not currently in session</td>
+	<td colspan='100' style='text-align:center'>The 2024 season is not currently in session</td>
 </tbody>
 `;
 document.getElementById('fe-constructors').innerHTML+=tabledata;
